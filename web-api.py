cdaa9d9beb0a4ddf8fb46cf179415e5285e71334
Luca D'Angelo <dangelo.luca@iisgalvanimi.edu.it>
2024-11-08 11:08:51 +0000

    commit 3

--- web-api.py
+++ web-api.py
@@ -30,5 +30,39 @@ def get_uccelli():
     return jsonify(uccelli)
 
 
+@app.route('/api/aggiungi/uccelli', methods=['POST'])
+def add_uccelli():
+    data = request.get_json()  # Recupera i dati JSON inviati dal client
+
+    if not all(key in data for key in ('Nome_Comune', 'famiglia', 'Apertura_Alare', 'Habitat', 'Alimentazione')):
+        return jsonify({"error": "Dati mancanti"}), 400
+
+    nome = data['Nome_Comune']
+    famiglia = data['famiglia']
+    apertura_alare = data['Apertura_Alare']
+    habitat = data['Habitat']
+    alimentazione = data['Alimentazione']
+
+    try:
+        
+        mydb = connect_to_db()
+        mycursor = mydb.cursor()
+
+        
+        sql = "INSERT INTO Uccelli (Nome_Comune, famiglia, Apertura_Alare, Habitat, Alimentazione) VALUES (%s, %s, %s, %s, %s)"
+        values = (nome, famiglia, apertura_alare, habitat, alimentazione)
+        
+        
+        mycursor.execute(sql, values)
+        mydb.commit()
+
+        mycursor.close()
+        mydb.close()
+
+        return jsonify({"message": "Animale inserito con successo!"}), 201
+    
+    except mysql.connector.Error as err:
+        return jsonify({"error": f"Errore nel database: {err}"}), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
